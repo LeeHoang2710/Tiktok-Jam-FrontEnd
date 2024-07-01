ab9629ecc4974db56a34df124ab94fc660c1099a
Chowkey <lekimchau211@gmail.com>
2024-07-01 21:39:52 +0700

    set up images, colors, and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#FD2C6D",
+        "secondary": "#F6CCD3",
+        "third": "#F0CD98",
+        "fourth": "#484E8C",
+        "radial": "#F6CCD3",
+      }
+    },
   },
   plugins: [],
 }
